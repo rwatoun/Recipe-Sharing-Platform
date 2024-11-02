d2c9ddf08b122c9d562279ec1a0e3ec98928833a
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-02 18:55:12 -0400

    Added a home page and a css file for its styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import logo from "./logo.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home";
-import Login from "./login";
+//import Login from "./login";
 import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+          // path="/login"
+          // element={<Login} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
+
+const Home = (props) => {
+  const { loggedIn, email } = props;
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+    // You'll update this function later
+  };
+
+  return (
+    <div className="mainContainer">
+      <div>
+        <div>Welcome to </div>
+        <div>R E C I P I</div>
+      </div>
+      <div>This is the home page.</div>
+      <div>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"LOG IN"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
