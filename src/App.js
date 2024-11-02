90bac70a2fa67cac383ce214383d925c9e0a5ebf
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-02 18:27:52 -0400

    installed the module react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import Login from "./login";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   return (
