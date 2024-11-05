7c35b11c29ff79cc41b0f63d47c9537216840895
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-04 21:04:57 -0500

    created react components to be able to reuse code

--- src/Login.js
+++ src/Login.js
@@ -4,47 +4,57 @@ import "./home.css";
 import logoo from "./logo.png";
 import { Link } from "react-router-dom";
 
-const Login = (props) => {
-  const navigate = useNavigate();
+function LeftContainer() {
+  return (
+    <div className={"authenImageContainer"}>
+      <div className={"authenLogo"}>
+        <Link to="/">
+          <img src={logoo} />
+        </Link>
+      </div>
+    </div>
+  );
+}
 
+function RightContainer() {
+  const navigate = useNavigate();
   const onButtonClick = () => {
     navigate("/Signup");
   };
+  return (
+    <div className={"authenFormContainer"}>
+      <form>
+        <label>Username:</label>
+        <br />
+        <input type="text" className="authenInput" />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input type="text" className="authenInput" />
+        <input type="submit" value={"Log in"} className={"loginButton"} />
+      </form>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div> Not a member? </div>
+      <div>
+        <input
+          className={"loginButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Sign up"}
+        />
+      </div>
+    </div>
+  );
+}
 
+const Login = (props) => {
   return (
     <div className="authenContainer">
-      <div className={"authenImageContainer"}>
-        <div className={"authenLogo"}>
-          <Link to="/">
-            <img src={logoo} />
-          </Link>
-        </div>
-      </div>
-      <div className={"authenFormContainer"}>
-        <form>
-          <label>Username:</label>
-          <br />
-          <input type="text" className="authenInput" />
-          <br />
-          <label>Password:</label>
-          <br />
-          <input type="text" className="authenInput" />
-          <input type="submit" value={"Log in"} className={"loginButton"} />
-        </form>
-        <br />
-        <br />
-        <br />
-        <br />
-        <div> Not a member? </div>
-        <div>
-          <input
-            className={"loginButton"}
-            type="button"
-            onClick={onButtonClick}
-            value={"Sign up"}
-          />
-        </div>
-      </div>
+      <LeftContainer />
+      <RightContainer />
     </div>
   );
 };
