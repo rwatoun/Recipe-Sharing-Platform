c767ebcf47108f99f407ee1bbc93cb0f1b5b18bc
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-07 12:05:37 -0500

    added a dropdown menu when hovering over recipes in home

--- src/navbar.js
+++ src/navbar.js
@@ -10,11 +10,28 @@ const Navbar = () => {
         <Link to="/">Recipi</Link>
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/recipes">Recipes</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/login" className="login-link">Login</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navItem">
+          <button className={"recipesButton"}>Recipes</button>
+          <div className="dropdownMenu">
+            <Link className="recipeTitle">Recipe 1</Link>
+            <Link className="recipeTitle">Recipe 2</Link>
+            <Link className="recipeTitle">Recipe 3</Link>
+          </div>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/login" className="login-link">
+            Login
+          </Link>
+        </li>
       </ul>
     </nav>
   );
