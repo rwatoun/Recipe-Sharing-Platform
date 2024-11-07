5356993c2724f05dbf5c524c571e58a321fed2d4
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-07 14:25:53 -0500

    corrected a small error about file path

--- src/about-us/about.js
+++ src/about-us/about.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar";
-import "/about.css";
+import "./about.css";
 
 function About() {
   return (
