5f4d1e72e958f5ba3c5b5c111d8f27f55bbdcca1
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-08 10:41:44 -0500

    created components for better code reuse in about page

--- src/about-us/about.js
+++ src/about-us/about.js
@@ -4,36 +4,52 @@ import Navbar from "../navbar";
 import "./about.css";
 import peopleCooking from "../peoplecooking.png";
 
+function FirstContent() {
+  return (
+    <div className={"firstContent"}>
+      <div className={"leftFirstContent"}>
+        <p style={{ fontSize: "10px", color: "#666", margin: "5px 0" }}>
+          {" "}
+          ABOUT US
+        </p>
+        <h1>nice to meet you.</h1>
+        <p>
+          Blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
+          blabla blabla, blabla blabla blabla blabla blabla. Blabla blabla
+          blabla blabla, blabla blabla blabla blabla. Blabla blabla blabla
+          blabla, blabla blabla blabla. Blabla blabla blabla blabla blabla
+          blabla blabla. Blabla blabla blabla blabla blabla, blabla blabla
+          blabla. Blabla blabla blabla blabla, blabla blabla blabla blabla
+          blabla. Blabla blabla blabla blabla blabla, blabla blabla blabla.
+          Blabla blabla blabla blabla blabla blabla blabla. Blabla blabla blabla
+          blabla blabla, blabla blabla blabla. Blabla blabla blabla blabla,
+          blabla blabla blabla blabla blabla blabla.{" "}
+        </p>
+        <button className={"signupButton"}>Sign up for free</button>
+      </div>
+      <div className={"rightFirstContent"}>
+        <img src={peopleCooking} />
+      </div>
+    </div>
+  );
+}
+
+function SecondContent() {
+  return (
+    <div className={"secondContent"}>
+      <div className={"leftSecondContent"}></div>
+      <div className={"rightSecondContent"}></div>
+    </div>
+  );
+}
+
 function About() {
   return (
     <div>
       <Navbar />
       <div className={"content"}>
-        <div className={"firstContent"}>
-          <div className={"leftFirstContent"}>
-            <h1>nice to meet you.</h1>
-            <p>
-              Blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
-              blabla blabla, blabla blabla blabla blabla blabla. Blabla blabla
-              blabla blabla, blabla blabla blabla blabla. Blabla blabla blabla
-              blabla, blabla blabla blabla. Blabla blabla blabla blabla blabla
-              blabla blabla. Blabla blabla blabla blabla blabla, blabla blabla
-              blabla. Blabla blabla blabla blabla, blabla blabla blabla blabla
-              blabla. Blabla blabla blabla blabla blabla, blabla blabla blabla.
-              Blabla blabla blabla blabla blabla blabla blabla. Blabla blabla
-              blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
-              blabla, blabla blabla blabla blabla blabla blabla.{" "}
-            </p>
-            <button className={"signupButton"}>Sign up for free</button>
-          </div>
-          <div className={"rightFirstContent"}>
-            <img src={peopleCooking} />
-          </div>
-        </div>
-        <div className={"secondContent"}>
-          <div className={"leftSecondContent"}></div>
-          <div className={"rightSecondContent"}></div>
-        </div>
+        <FirstContent />
+        <SecondContent />
       </div>
     </div>
   );
