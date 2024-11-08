c7cacc19bf17ff47148f11d8787e9f0951c7acdd
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-08 15:15:48 -0500

    continued working on the ui of the about us page

--- src/about-us/about.js
+++ src/about-us/about.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar";
 import "./about.css";
-import peopleCooking from "../peoplecooking.png";
-import third from "../third.jpg";
+import peopleCooking from "../people-cooking.png";
+import image1 from "../image1.jpeg";
+import image2 from "../image2.jpg";
+import image3 from "../image3.jpg";
+import image4 from "../image4.jpg";
 
 function FirstContent() {
+  const navigate = useNavigate();
+  const toSignup = () => {
+    navigate("/Signup");
+  };
   return (
     <div className={"firstContent"}>
       <div className={"leftFirstContent"}>
@@ -26,7 +33,9 @@ function FirstContent() {
           blabla blabla, blabla blabla blabla. Blabla blabla blabla blabla,
           blabla blabla blabla blabla blabla blabla.{" "}
         </p>
-        <button className={"signupButton"}>Sign up for free</button>
+        <button onClick={toSignup} className={"signupButton"}>
+          Sign up for free
+        </button>
       </div>
       <div className={"rightFirstContent"}>
         <img src={peopleCooking} />
@@ -78,7 +87,69 @@ function SecondContent() {
 }
 
 function ThirdContent() {
-  return <div className={"thirdContainer"}></div>;
+  return <div className={"thirdContent"}></div>;
+}
+
+function FourthContent() {
+  const [displayContent, setDisplayContent] = useState({
+    text: "Text for Button 1",
+    image: image1,
+  });
+
+  const [selectedButton, setSelectedButton] = useState(1);
+
+  // Content for each button
+  const contentMap = {
+    1: { name: "Recipe 1", text: "Text for Button 1", image: image1 },
+    2: { name: "Recipe 2", text: "Text for Button 2", image: image2 },
+    3: { name: "Recipe 3", text: "Text for Button 3", image: image3 },
+    4: { name: "Recipe 4", text: "Text for Button 4", image: image4 },
+  };
+
+  // Function to handle button clicks and set the content
+  const handleButtonClick = (buttonId) => {
+    setDisplayContent(contentMap[buttonId]);
+    setSelectedButton(buttonId); // Update the selected button ID
+  };
+
+  return (
+    <div className={"fourthContent"}>
+      <div className={"topFourthContent"}>
+        <p style={{ fontSize: "3rem", fontWeight: "bold" }}>
+          The story of Recipi
+        </p>
+        <p style={{ alignSelf: "flex-start" }}>
+          Blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
+          blabla blabla, blabla blabla blabla blabla blabla. Blabla blabla
+          blabla blabla, blabla blabla blabla blabla. Blabla blabla blabla
+          blabla, blabla blabla blabla.
+        </p>
+      </div>
+      <div className={"bottomFourthContent"}>
+        <div className={"leftBottomFourthContent"}>
+          {Object.keys(contentMap).map((id) => (
+            <button
+              key={id}
+              className={`buttonFourthContent ${
+                selectedButton === parseInt(id) ? "selected" : ""
+              }`}
+              onClick={() => handleButtonClick(parseInt(id))}
+            >
+              {contentMap[id].name}
+            </button>
+          ))}
+        </div>
+        <div className={"rightBottomFourthContent"}>
+          <div className={"imgRightBottomFourthContent"}>
+            <img src={displayContent.image} />
+          </div>
+          <div className={"textRightBottomContent"}>
+            <p>{displayContent.text}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function About() {
@@ -89,6 +160,7 @@ function About() {
         <FirstContent />
         <SecondContent />
         <ThirdContent />
+        <FourthContent />
       </div>
     </div>
   );
