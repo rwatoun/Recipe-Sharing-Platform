973ebf135487fe3568b50f1ceb6ce931dbe8fd89
Marwa Hammani <hammanimarwa0@gmail.com>
2024-11-07 19:23:41 -0500

    worked on the first container in the about us page

--- src/about-us/about.js
+++ src/about-us/about.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../navbar";
 import "./about.css";
+import peopleCooking from "../peoplecooking.png";
 
 function About() {
   return (
@@ -9,8 +10,25 @@ function About() {
       <Navbar />
       <div className={"content"}>
         <div className={"firstContent"}>
-          <div className={"leftFirstContent"}></div>
-          <div className={"rightFirstContent"}></div>
+          <div className={"leftFirstContent"}>
+            <h1>nice to meet you.</h1>
+            <p>
+              Blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
+              blabla blabla, blabla blabla blabla blabla blabla. Blabla blabla
+              blabla blabla, blabla blabla blabla blabla. Blabla blabla blabla
+              blabla, blabla blabla blabla. Blabla blabla blabla blabla blabla
+              blabla blabla. Blabla blabla blabla blabla blabla, blabla blabla
+              blabla. Blabla blabla blabla blabla, blabla blabla blabla blabla
+              blabla. Blabla blabla blabla blabla blabla, blabla blabla blabla.
+              Blabla blabla blabla blabla blabla blabla blabla. Blabla blabla
+              blabla blabla blabla, blabla blabla blabla. Blabla blabla blabla
+              blabla, blabla blabla blabla blabla blabla blabla.{" "}
+            </p>
+            <button className={"signupButton"}>Sign up for free</button>
+          </div>
+          <div className={"rightFirstContent"}>
+            <img src={peopleCooking} />
+          </div>
         </div>
         <div className={"secondContent"}>
           <div className={"leftSecondContent"}></div>
