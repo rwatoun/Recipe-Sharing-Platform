3b8e1086687d0f884797956f3b48a399bb5e2d32
Marwa Hammani <hammanimarwa0@gmail.com>
2024-12-10 19:20:35 -0500

    worked on the skeleton of the home page

--- src/Containers/Home/home.js
+++ src/Containers/Home/home.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../Components/Navbar/navbar";
 import Footer from "../Footer/footer";
-import logo from "../../Components/Logo/logo.png";
+import redirection1 from "./redirection1.jpg";
 import "./home.css";
 import { Link } from "react-router-dom";
 
@@ -17,17 +17,17 @@ const Home = () => {
   return (
     <div className="home-page">
       <Navbar />
-      <header className="hero-section">
-        <h1>Welcome to </h1>
-        <div className={"loginLogo"}>
-          <Link to="/">
-            <img src={logo} />
-          </Link>
-        </div>
-        <p>Discover, share, and enjoy recipes from around the world!</p>
-      </header>
-
-      <section className="featured-recipes">
+      <div className="hero-section">
+        <h1 style={{ fontSize: "60px" }}>
+          {" "}
+          Having a <span style={{ fontSize: "90px" }}>recipi </span>is always
+          better{" "}
+        </h1>
+        <h1 style={{ fontSize: "40px" }}>
+          Discover, Taste, and Share Traditions of the World
+        </h1>
+      </div>
+      <div className="featured-recipes">
         <h2>Featured Recipes</h2>
         <div className="recipe-cards">
           <div className="recipe-card">
@@ -43,7 +43,102 @@ const Home = () => {
             <h3>Recipe Name 3</h3>
           </div>
         </div>
-      </section>
+      </div>
+      <div className="trap1">
+        <div className="trap1-img">
+          {" "}
+          <img src={redirection1} />
+        </div>
+        <div className="trap1-text">
+          <h1>You made it, you own it.</h1>
+          <p>
+            Your recipes are yours, and they stay that way. You always own your
+            culinary creations, subscriber list, and community connections. With
+            full creative freedom and no intermediaries, you can share your
+            passion for cooking just the way you want.
+          </p>
+          <a>Start Sharing Your Recipes</a>
+        </div>
+      </div>
+      <div className="trap1">
+        <div className="trap1-img">
+          {" "}
+          <img src={redirection1} />
+        </div>
+        <div className="trap1-text">
+          <h1>Grow your culinary community.</h1>
+          <p>
+            Building your audience doesn’t have to be a solo effort. Our
+            platform fosters a network of food lovers, ensuring your recipes
+            reach the people who’ll love them most. Plus, recommendations and
+            community-driven sharing help your following grow effortlessly.
+          </p>
+          <a>Start Sharing Your Recipes</a>
+        </div>
+      </div>
+      <div className="trap1">
+        <div className="trap1-img">
+          {" "}
+          <img src={redirection1} />
+        </div>
+        <div className="trap1-text">
+          <h1>Let us handle the rest.</h1>
+          <p>
+            Our Recipe Sharing Platform takes care of everything: from hosting
+            your recipe pages and showcasing stunning food photos to handling
+            subscriptions and support. With an easy-to-use interface, you can
+            focus on sharing your love for food while we manage the details.
+          </p>
+          <a>Start Sharing Your Recipes</a>
+        </div>
+      </div>
+      <div className="redirection">
+        <div className="redirection-title">
+          <h1 style={{ fontSize: "70px" }}> a world class platform</h1>
+        </div>
+        <div className="redirections">
+          <div className="redirection1">
+            <div className="redirection1-text">
+              <p>
+                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla
+              </p>
+            </div>
+            <div className="redirection1-img">
+              <img src={redirection1} />
+            </div>
+          </div>
+          <div className="redirection1">
+            <div className="redirection1-img">
+              <img src={redirection1} />
+            </div>
+            <div className="redirection1-text">
+              <p>
+                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla
+              </p>
+            </div>
+          </div>
+          <div className="redirection1">
+            <div className="redirection1-text">
+              <p>
+                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla
+              </p>
+            </div>
+            <div className="redirection1-img">
+              <img src={redirection1} />
+            </div>
+          </div>
+          <div className="redirection1">
+            <div className="redirection1-img">
+              <img src={redirection1} />
+            </div>
+            <div className="redirection1-text">
+              <p>
+                blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
